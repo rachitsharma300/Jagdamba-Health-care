3b0ccd832eaeafe0e0cf24a055d5cd437b4b2b53
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-24 23:22:13 +0530

    Feat: Dependecy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+    primary: "#00695c",    // Teal dark (Healthcare trust color)
+    secondary: "#26a69a",  // Teal light (Accent)
+    accent: "#f2f7fb",
+    dark: "#333333",
+      },
+    },
   },
   plugins: [],
 }
-
